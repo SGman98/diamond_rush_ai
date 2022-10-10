5962b4a18cc87b51b83e1271d3e9e434b258940e
SGman98 <bastiangarnica@gmail.com>
2022-10-09 22:37:55 -0500

    Add function to crop black borders from image

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 from tkinter import filedialog
 
@@ -11,3 +12,16 @@ def show_image(image):
     cv.imshow('image', image)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+
+def crop_black_borders(image):
+    # apply filters
+    blurred = cv.blur(image, (3, 3))
+    canny = cv.Canny(blurred, 100, 200)
+
+    # find the non-zero min-max coords of canny
+    pts = np.argwhere(canny > 0)
+    y1, x1 = pts.min(axis=0)
+    y2, x2 = pts.max(axis=0)
+
+    return image[y1:y2, x1:x2]
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from image_processing import get_image, show_image
+from image_processing import crop_black_borders, get_image, show_image
 
 
 def main():
     im = get_image()
+    im = crop_black_borders(im)
 
     show_image(im)
 
