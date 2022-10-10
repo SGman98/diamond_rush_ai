ea6d7d092a31cd36a42997b0e63078adfb7a1cec
SGman98 <bastiangarnica@gmail.com>
2022-10-09 22:52:34 -0500

    Add function to crop unnecessary tiles

--- src/image_processing.py
+++ src/image_processing.py
@@ -25,3 +25,15 @@ def crop_black_borders(image):
     y2, x2 = pts.max(axis=0)
 
     return image[y1:y2, x1:x2]
+
+
+def crop_img(image, cols, rows, top=0, bottom=0, left=0, right=0):
+    height, width = image.shape[:2]
+    tile_width = width / cols
+    tile_height = height / rows
+    # remove top and bottom
+    image = image[int(top * tile_height):int((rows - bottom) * tile_height), :]
+    # remove left and right
+    image = image[:, int(left * tile_width):int((cols - right) * tile_width)]
+
+    return image
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from image_processing import crop_black_borders, get_image, show_image
 def main():
     im = get_image()
     im = crop_black_borders(im)
+    im = crop_img(im, 10, 15, top=2, bottom=1, left=1, right=1)
 
     show_image(im)
 
