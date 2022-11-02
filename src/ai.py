56a2c99c3d816e661b2b0538b92ee047014724ae
SGman98 <bastiangarnica@gmail.com>
2022-11-01 19:56:44 -0500

    Add logic to AI using "brute force"

--- src/ai.py
+++ src/ai.py
@@ -22,10 +22,22 @@ def get_movement_from_array(arr):
     for row in range(len(arr)):
         for col in range(len(arr[row])):
             if arr[row][col] == "#":
+                # arr[row][col] = "P"
                 start = (row, col)
             elif arr[row][col] == "E":
+                # arr[row][col] = "P"
                 end = (row, col)
 
-    movement = graph.get_path(arr, start, end)
+    # movement = graph.get_path(arr, start, end)
+    player = graph.Player(arr, start, end)
+
+    # solve
+    result = player.solve()
+
+    # get movement
+    if result:
+        movement = player.movement
+    else:
+        movement = []
 
     return ''.join(movement)
--- src/graph.py
+++ src/graph.py
@@ -2,6 +2,8 @@ from queue import PriorityQueue
 
 import numpy as np
 
+LOGGING = True
+
 
 class Spot:
     def __init__(self, x, y, rows, cols):
@@ -111,6 +113,9 @@ class Spot:
             'B': self.make_button,
             'S': self.make_spike,
             'R': self.make_rock,
+
+            'E': lambda: None,
+            '#': lambda: None,
         }
 
         if (value in transform):
@@ -158,15 +163,15 @@ class Spot:
         return f'Spot({self.x}, {self.y})'
 
 
-# Manhattan distance
-def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
 def astar(grid, start, end):
+
+    # Manhattan distance
+    def h(p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+
+        return abs(x1 - x2) + abs(y1 - y2)
+
     count = 0
 
     open_set = PriorityQueue()
@@ -221,7 +226,7 @@ def astar(grid, start, end):
 def path_to_movement(path):
     movement = []
     if path is None:
-        print('No path found')
+        # print('No path found in path_to_movement')
         return movement
     for i in range(len(path) - 1):
         x1, y1 = path[i].get_pos()
@@ -239,30 +244,143 @@ def path_to_movement(path):
     return movement
 
 
-def get_path(board, start, end):
-    # make grid
-    grid = []
+class Player:
+    def __init__(self, board, start, end, depth=0):
+        self.board = board
+        self.pos = start
+        self.end = end
+        self.depth = depth
+        self.has_key = False
+        self.diamonds = self.get_total_diamonds()
+        self.movement = []
+        self.logging = False
 
-    for i, row in enumerate(board):
-        grid.append([])
+    def get_total_diamonds(self):
+        total_diamonds = 0
 
-        for j, spot in enumerate(row):
-            spot = Spot(i, j, len(board), len(row))
+        for row in self.board:
+            for spot in row:
+                if spot == 'D':
+                    total_diamonds += 1
 
-            spot.make_state(board[i][j])
+        return total_diamonds
 
-            grid[i].append(spot)
+    def get_interest_points(self):
+        interest_points = []
 
-    for row in grid:
-        for spot in row:
-            spot.update_neighbors(grid)
+        for i, row in enumerate(self.board):
+            for j, spot in enumerate(row):
+                if self.get_path((i, j)) == []:
+                    continue
 
-    # get path
-    path = astar(grid, grid[start[0]][start[1]], grid[end[0]][end[1]])
+                if self.diamonds == 0 and spot == 'E':
+                    interest_points.append((i, j))
+                elif not self.has_key and spot == 'K':
+                    interest_points.append((i, j))
+                elif spot == 'D':
+                    interest_points.append((i, j))
 
-    # transform path to movement W, A, S, D
-    movement = path_to_movement(path)
+        return interest_points
 
-    print(movement)
+    def get_path(self, end):
+        grid = []
 
-    return movement
+        for i, row in enumerate(self.board):
+            grid.append([])
+
+            for j, spot in enumerate(row):
+                spot = Spot(i, j, len(self.board), len(row))
+
+                spot.make_state(self.board[i][j])
+
+                grid[i].append(spot)
+
+        for row in grid:
+            for spot in row:
+                spot.update_neighbors(grid)
+
+        # get path
+        start = grid[self.pos[0]][self.pos[1]]
+        path = astar(grid, start, grid[end[0]][end[1]])
+
+        # transform path to movement W, A, S, D
+        movement = path_to_movement(path)
+
+        return movement
+
+    def move(self, end):
+        self.movement = self.get_path(end)
+
+        for direction in self.movement:
+            if direction == 'w':
+                self.pos = (self.pos[0] - 1, self.pos[1])
+            elif direction == 's':
+                self.pos = (self.pos[0] + 1, self.pos[1])
+            elif direction == 'a':
+                self.pos = (self.pos[0], self.pos[1] - 1)
+            elif direction == 'd':
+                self.pos = (self.pos[0], self.pos[1] + 1)
+
+            self.update_state()
+
+    def update_state(self):
+        if self.board[self.pos[0]][self.pos[1]] == 'K' and not self.has_key:
+            self.has_key = True
+            self.board[self.pos[0]][self.pos[1]] = 'P'
+            return
+        if self.board[self.pos[0]][self.pos[1]] == 'D':
+            self.diamonds -= 1
+            self.board[self.pos[0]][self.pos[1]] = 'P'
+            return
+        if self.board[self.pos[0]][self.pos[1]] == 'S':
+            self.board[self.pos[0]][self.pos[1]] = 'W'
+            return
+
+    def print(self, message):
+        global LOGGING
+        if not LOGGING:
+            return
+        print(">\t"*self.depth + message)
+
+    def board_to_string(self):
+        return '\n'.join([''.join(row) for row in self.board])
+
+    def solve(self):
+        global LOGGING
+        if self.pos[0] == 10 and self.depth == 1:
+            LOGGING = True
+        self.print(f"New Player from position {self.pos[0]}, {self.pos[1]}")
+        interest_points = self.get_interest_points()
+
+        result_ = False
+
+        for point in interest_points:
+            board_copy = self.board.copy()
+            new_player = Player(board_copy, self.pos, self.end, self.depth+1)
+
+            new_player.move(point)
+            self.print(f"Get {point} movement {''.join(new_player.movement)}")
+
+            if new_player.pos == self.pos:
+                self.print('No path found')
+                result_ = False
+                break
+
+            if new_player.pos == new_player.end:
+                self.print("Player reached the end")
+                self.movement += new_player.movement
+
+                result_ = True
+                break
+
+            if new_player.solve():
+                self.movement += new_player.movement
+                if (self.depth == 0):
+                    self.print(f"Result: {''.join(self.movement)}")
+                result_ = True
+                break
+
+        if not result_:
+            self.print(f"Player failed to reach the end")
+
+        return result_
